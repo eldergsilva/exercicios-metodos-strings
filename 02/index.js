86e9bcffb9cddfa67c1db591be1d52c06545111a
eldergsilva <eldergsilva@gmail.com>
2026-03-30 17:24:00 -0300

    exercicio 02 cnpj

--- 02/index.js
+++ 02/index.js
@@ -1,5 +1,6 @@
 const cpf = "12345678900";
- 
+const cnpj = "12345678000199";
+
 function cpfFormatado(cpf) { 
       
     if (cpf.length !== 11) {
@@ -16,4 +17,14 @@ function cpfFormatado(cpf) {
     }
 }
 
-cpfFormatado(cpf);
+function cnpjFormatado(cnpj){
+        let parte1 = cnpj.slice(0,2);   
+        let parte2 = cnpj.slice(2,5);   
+        let parte3 = cnpj.slice(5,8);
+        let parte4 = cnpj.slice(8,12);          
+        let digito = cnpj.slice(-2);
+        console.log(`${parte1}.${parte2}.${parte3}/${parte4}-${digito}`);        
+}
+
+cpfFormatado(cpf) 
+cnpjFormatado(cnpj)
