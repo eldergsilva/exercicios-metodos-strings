1a6de79765f3cbddbe21f8ba98bbfcfcb168be5f
eldergsilva <eldergsilva@gmail.com>
2026-03-30 10:13:41 -0300

    exercicio 02 cpf

--- 02/index.js
+++ 02/index.js
@@ -1,8 +1,19 @@
 const cpf = "12345678900";
-const cnpj ="";
-
-if( cpf.length < 11){
-     console.log(`CPF Inválido`);     
-}else{
-    // codigo
+ 
+function cpfFormatado(cpf) { 
+      
+    if (cpf.length !== 11) {
+        console.log("CPF Inválido");
+    } else {
+        
+        let parte1 = cpf.slice(0, 3);   
+        let parte2 = cpf.slice(3, 6);   
+        let parte3 = cpf.slice(6, 9);   
+        
+        let digito = cpf.slice(-2); 
+        
+        console.log(`${parte1}.${parte2}.${parte3}-${digito}`);
+    }
 }
+
+cpfFormatado(cpf);
